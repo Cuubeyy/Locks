833b16831a4ba88725e6cd68c8f8fee26eb4c6be
eml <emil.d@hlke.eu>
2023-03-08 12:14:05 +0100

    Moved LockInventoryConfigurationListener to ConfigurationInventory

--- src/main/java/de/cubedude/locks/inventorys/ConfigurationInventory.java
+++ src/main/java/de/cubedude/locks/inventorys/ConfigurationInventory.java
@@ -1,13 +1,19 @@
 package de.cubedude.locks.inventorys;
 
+import de.cubedude.locks.utils.ConfigManager;
 import de.cubedude.locks.utils.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import sun.security.krb5.Config;
 
 import javax.swing.text.Position;
 import java.lang.reflect.Array;
@@ -16,7 +22,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ConfigurationInventory {
+public class ConfigurationInventory implements Listener {
+
+    private final ConfigManager config;
+
+    public ConfigurationInventory(ConfigManager config) {
+        this.config = config;
+    }
 
     public static Inventory createInventory(Player player, List<String> lore, Location location, int mode) {
         Inventory inventory = Bukkit.createInventory(null, 54, "Lock Configuration");
@@ -82,4 +94,54 @@ public class ConfigurationInventory {
         return inventory;
     }
 
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        String path = "";
+        int mode;
+        if (event.getView().getTitle().equals("Lock Configuration")) mode = 1;
+        else if (event.getView().getTitle().startsWith("Lock Configuration")) {
+            path = event.getView().getTitle().replace("Lock Configuration", "");
+            mode = 2;
+        } else return;
+
+        Player player = (Player) event.getView().getPlayer();
+        ItemStack clickedItem = event.getCurrentItem();
+        ItemStack item;
+
+        if (clickedItem == null || clickedItem.getType() != Material.PLAYER_HEAD) {
+            event.setCancelled(true);
+            return;
+        }
+        if (mode == 1) {
+            if (player.getInventory().getItemInMainHand().getType() == Material.IRON_INGOT &&
+                    player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.YELLOW + "Lock"))
+                item = player.getInventory().getItemInMainHand();
+            else item = player.getInventory().getItemInOffHand();
+        } else {
+            List<String> players = (List<String>) config.getConfig().get(path + ".owners");
+            item = new ItemStack(Material.IRON_INGOT);
+            item.setLore(players);
+        }
+
+        List<String> lore = item.getLore();
+        String name = clickedItem.getItemMeta().getDisplayName();
+
+        assert lore != null;
+        if (lore.contains(name)) {
+            lore.remove(name);
+            clickedItem.setLore(Collections.singletonList("Click to add this player to the lock."));
+        }
+        else {
+            lore.add(name);
+            clickedItem.setLore(Collections.singletonList("Click to remove this player from the lock."));
+        }
+
+        item.setLore(lore);
+
+        if (mode == 2) {
+            config.getConfig().set(path + ".owners", item.getLore());
+        }
+
+        event.setCancelled(true);
+    }
 }
